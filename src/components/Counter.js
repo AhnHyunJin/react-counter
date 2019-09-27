1ceffe31d740d940a7721902268b21982f6ef0ec
DESKTOP-DI0HS5K\hjan1 <hjan7804@gmail.com>
2019-09-27 12:34:57 +0900

    handle state with Buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
 class Counter extends React.Component{
-    
+    constructor(props){
+        super(props)
+        this.state = {
+            num : 0
+        }
+    }
 
     handlePlusButton = () =>{
+        this.setState({
+            num : this.state.num + 1
+        })
+    }
 
+    handleMinusButton = () =>{
+        this.setState({
+            num : this.state.num - 1
+        })
     }
 
     render(){
         return(
             <div>
                 <h1>counter</h1>
-                <button>+</button>
-                <button>-</button>
+                <button onClick={this.handlePlusButton}>+</button>
+                <button onClick={this.handleMinusButton}>-</button>
+                <h2>result : {this.state.num} </h2>
             </div>
         );
     }
