5c899edca1c3ee439e4257f766460a1f7e2cae2e
DESKTOP-DI0HS5K\hjan1 <hjan7804@gmail.com>
2019-09-27 12:18:55 +0900

    make buttons

---
+++ src/components/Counter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Counter extends React.Component{
+    
+
+    handlePlusButton = () =>{
+
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>counter</h1>
+                <button>+</button>
+                <button>-</button>
+            </div>
+        );
+    }
+}
+
+export default Counter
